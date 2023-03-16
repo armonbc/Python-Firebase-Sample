5d099d127fbcb5313ce0207cb12f655fe3931fc3
Armon C <121739826+armonbc@users.noreply.github.com>
2023-03-16 19:09:21 +0800

    Armonbc Yolo (#10)
    * Create firebasetest.py
    
    * Delete firebasetest.py
    
    * Update pythontest.yml
    
    * Update firebasetest.py
    
    * Update README.md
    
    * Create firebasetest.py
    
    * Update firebasetest.py
    
    * Create firebasetest.py
    
    * Update firebasetest.py
    
    * Update pythontest.yml
    
    * Update pythontest.yml
    
    * Update pythontest.yml

---
+++ Delete/firebasetest.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import os
+import json
+
+my_secret_cred = os.environ.get('FIREBASE_CRED')
+my_secret_db_url = os.environ.get('DB_URL')
+
+
+cred = credentials.Certificate(json.loads(my_secret_cred))
+firebase_admin.initialize_app(cred, {
+    'databaseURL': my_secret_db_url
+})
+
+# Get a reference to the root node of your database
+ref = db.reference('/users')
+
+# Delete all data under the '/users' node
+ref.delete()
+print(snapshot)
