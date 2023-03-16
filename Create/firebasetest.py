303aa9653766ef013e5a0a904e40bcfe7d8dd5cc
Armon C <121739826+armonbc@users.noreply.github.com>
2023-03-16 10:57:55 +0800

    Update firebasetest.py

--- Create/firebasetest.py
+++ Create/firebasetest.py
@@ -2,12 +2,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import os
+import json
 
 my_secret_cred = os.environ.get('FIREBASE_CRED')
 my_secret_db_url = os.environ.get('DB_URL')
 
 
-cred = credentials.Certificate.from_dict(json.loads(my_secret_cred))
+cred = credentials.Certificate(json.loads(my_secret_cred))
 firebase_admin.initialize_app(cred, {
     'databaseURL': my_secret_db_url
 })
